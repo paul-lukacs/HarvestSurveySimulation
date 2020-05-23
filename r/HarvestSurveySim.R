222a452bef369df509f084e94e0d7f03c56f5cc4
donati-pd <65191186+donati-pd@users.noreply.github.com>
2020-05-22 18:02:02 -0600

    Adding follow up surveys
    - Formatting changes, comment updates.
    - Combined both functions into one
    - Added more checks for arguments
    - Replaced complicated for loops in some cases, added more in others...
    - Converted survey matrices into dataframes
    - Changed names for response sims to reflect BOTH prob of succ. & unsucc. hunters responding.
    - Simulated follow up surveys
    - Changed outputs of function

--- r/HarvestSurveySim.R
+++ r/HarvestSurveySim.R
@@ -1,162 +1,174 @@
-# Hunter Simulation
-#
-#
-#
-#
-########## Population generator function ######################################
+########## Population generator function, with examples ##########
 
-# The below function simulates a population and returns a list with 3 elements. 
-# 1st element is a dataframe containing the entire population. 
-# 2nd element is a matrix containing sampled hunters only
-# 3rd is the number of total harvests by the simulated population. 
-# Response is not calculated by this function
+# The below function simulates a population.
+# It simulates harvest (after pop is split into two evenly sized groups of differing success rates), If they were selected for a survey, and if they responded, including follow up surveys
+# The function returns a list with 3 + X elements
+# 1st element is the total population size (a number)
+# 2nd element is the true harvest of entire population (also a number)
+# 3rd element is a dataframe of the complete population, but with blank survey columns.
+# X more elements for as many levels of response simulated there are.
 
 # ARGUMENTS:
 # n         = pop size
 # pSuccess1 = prob. of hunter group 1 harvesting
 # pSuccess2 = prob. of hunter group 2 harvesting. Is equal to pSuccess1 by default (for scenarios where all hunters are equally likely to harvest)
-# pSample   = probability a hunter is sampled.
+# pSample   = probability a hunter is sampled for original survey
+# suc       = a seq() that dictates response rates of successful hunters
+# uns       = same as "suc" by default, but is also seq() of response rates for unsuccessful hunters if specified
+# FUS       = 0 by default, but can be specified to indicate how many follow ups to conduct
+# FUSscale  = scales original "suc" and "uns" arguments (i.e. suc * FUSscale). Default is 1, meaning if not specified everyone re-sampled will respond w/ the same prob. as originally defined in suc and uns. It rescales for each consecutive follow up. 
 
-popgen <- function (n, pSuccess1, pSuccess2 = pSuccess1, pSample){  
- 
+popgen <- function (n, pSuccess1, pSuccess2 = pSuccess1, pSample, suc, uns = suc, FUS = 0, FUSscale = 1){
   
-  pop <- matrix (NA, nrow = n, ncol = 4)		  # Creating initial matrix with row length n.
-  colnames(pop) <- c("Group", "Harvest", "Sampled", "Response")
-  
-  for (i in 1:n){
-    
-    if (i <= n/2){                            # Splitting pop. in half.
-      pop [i, 1] <- 1                         # Assigning first half to one of two harvest groups
-    }                                         
-    else {
-      pop [i, 1] <- 0                         # Assigning second half to other group.
-    }
-    
-      pop [i, 3] <- rbinom (1, 1, pSample)    # Sampled or not?
-  }
-  
-  
-  
-  for (i in 1:n){
-    if (pop [i, 1] == 1) {
-      pop [i, 2] <- rbinom (1, 1, pSuccess1)  # If hunter is in group 1, assign harvest based on pSuccess1
-    }
-    else{
-      pop [i, 2] <- rbinom (1, 1, pSuccess2)  # If hunter is in group 2, assign harvest based on pSuccess2, which could be equal to pSuccess1 if argument left blank.
-    }
-  }
-  
-  
-######## OUTPUTS
-  
-  
-  TrueHvst <- sum (pop [ , 2])                                                # True harvest of entire pop.
-  sampled <- subset (pop, pop[ , 3] == TRUE)                                  # Sampled hunters only, into new matrix
-  list <- (list(as.data.frame(pop), sampled, TrueHvst))                       # Creating the list with 3 elements
-  names(list) <- c("total population", "sampled population", "true harvest")
-  return(list)                                                                
-}
-
-########## Survey generator function ##########
-
-# This will return an array with each consecutive third dimension being a simulation of (cont.)
-# survey response by increasing levels based on the inputs for arguments "suc" and "uns". 
-# sequences for these two arguments must be of equal length and be of length 2-10
-
-# ARGUMENTS: 
-# "x"   = a list, from popgen() output
-# "suc" = a seq() that dictates response rates of successful hunters
-# "uns" = same as "suc" by default, but is also seq() of response rates for unsuccessful hunters if specified
-
-survgen <- function (x, suc, uns = suc){
-  
-  
-#### First some quick checks to make sure sequences were inputted correctly:
-  
+  # First some quick checks to make sure arguments were inputted correctly:
   if (length (suc) > 10 || length (uns) >10 || length (suc) < 2 || length (uns) < 2 ){
-    stop("Lengths of sequences too long or short (must be between 2 and 10)")
+    stop("Length of 'suc' or 'uns' too long or short (must be between 2 and 10)")
+  }
+  if (FUSscale <= 0 || FUSscale > 1){
+    stop("FUSscale must be larger than 0 and less than or equal to 1")
+  }
+  if (pSample <= 0 || pSample > 1){
+    stop("pSample must be larger than 0 and less than or equal to 1")
+  }
+  if (pSuccess1 <= 0 || pSuccess1 > 1){
+    stop("pSuccess1 must be larger than 0 and less than or equal to 1")
+  }
+  if (pSuccess2 <= 0 || pSuccess2 > 1){
+    stop("pSuccess2 must be larger than 0 and less than or equal to 1")
   }
   
   if (length (suc) != length (uns)){
     stop ("length of 'suc' & 'uns' must be of equal length")
   }
   
+  # Create initial matrix to eventually hold all population data
+  pop <- matrix (NA, nrow = n, ncol = 4)		                # Creating initial matrix with row length n.
+  columns <- c("Group", "Harvest", "Sampled", "Response")
+  colnames(pop) <- columns
+  
+  if (FUS > 0){                                             # Make columns for any follow up surveys
+    FUSmat <- matrix(NA, nrow = n, ncol = FUS)
+    colnames(FUSmat) <- paste ("FUS", seq(1, FUS, 1))       # Name them
+    pop <- cbind(pop, FUSmat)                               # Add them
+  }
+  
+  # Start filling in matrix "pop"
+  z <- row(pop)                                             # Extracting row numbers from matrix "pop"
+  z <- z[ ,1]                                               # Turning it into a vector containing row numbers
+  
+  pop [ , 1] <- ifelse (z <= n/2,
+                        1,
+                        0)                                  # Fill in "Group" column with 1st half of pop. = 1 and 2nd half = 0
+  
+  pop [ , 2] <- ifelse (pop [ , 1] == 1, 
+                        rbinom (length(pop), 1, pSuccess1), 
+                        rbinom (length(pop), 1, pSuccess2)) # Simulate "Harvest" for both groups
+  
+  pop [ , 3] <- rbinom (length(z), 1, pSample)              # Sampled or not?
+  sampled <- subset (pop, pop [ , 3] == TRUE)               # Creating new matrix of sampled hunters only
   
   
-#### Now to create the array with row and column length of sampled hunters, and replicate by as many terms as in length(suc)
+  # Simulate hunter response to initial surveys.
+  init.surv <- array(sampled, dim = c(nrow(sampled), ncol(sampled), length(suc)))  
   
-  samp <- array(x[[2]], dim = c(nrow(x[[2]]), ncol(x[[2]]), length(suc)))  
-  
-  colnames(samp) <- c("Group", "Harvest", "Sampled", "Response")
-  dimnames(samp)[[3]] <- c(as.character(suc))   # Adding reporting %'s for easier recall
-  y <- x[[2]][ ,2]                              # Extracting whether hunter had successful harvest or not to be (cont.)
-                                                # later evaluated if T or F when assigning response rates below.
+  colnames(init.surv) <- colnames(pop)
+  init.surv.names <- paste(suc, uns)                  # Adding reporting prob. for easier recall. 
+  dimnames(init.surv)[[3]] <- init.surv.names         # Third dimensions will be named by their respective responding rates with 1st number (cont.)
+  # being prob. succ. hunter responds and 2nd number being prob. unsucc. hunter responds (e.g. "0.8 0.6")
   
   
-#### Now to fill in the array:
   
-  for (j in 1:nrow(samp)){
-    if (y [j] == 1){
-      for (k in 1:length(suc)){
-        samp [j , 4, k] <- rbinom (1, 1, suc[k])  # For all layers of array apply response based off prob. suc[k] (haha)
-      }                                           # suc[k] = prob. that a successful hunter responds based on given seq() for argument "suc" at position k
-      
+  # Now to fill in the array's "response" column:
+  y <- sampled [ ,2]                                  # Extracting whether sampled hunter had successful harvest or not
+  
+  for (j in 1:nrow(init.surv)){
+    if (y [j] == 1){                                      # If hunter had successful harvest, do this, otherwise go to else statement.
+      init.surv [j, 4, ] <- rbinom (length(suc), 1, suc)  # use vectorized rbinom to generate a vector of trials and save the whole vector to samp[j,4, ] (note the third subscript is left out to allow the vector to be added).                                        
     }
     else {
-      for (k in 1:length(suc)){
-        samp [j , 4, k] <- rbinom (1, 1, uns[k])  # For first layer of array apply response based off prob. uns[k] (prob. that an unsuccessful hunter responds.)
-      }
+      init.surv [j, 4, ] <- rbinom (length(uns), 1, uns)  # Now do the same for unsuccessful hunters.
     }
   }
-  return(samp)
+  
+  # Conduct any follow up surveys:
+  
+  if (FUS > 0){                                                                 # If follow up surveys are to be done, do 'em
+    for (j in 1:FUS-1){                                                         # Ok, confusing, but starts at 0 (NOT 1!) and ends at FUS-1
+      suc <- suc*FUSscale                                                       # For each follow up survey, the next one will be less likely to be responded to.
+      uns <- uns*FUSscale
+      for (i in 1:(nrow(init.surv))){                       
+        init.surv[i, 5+j, ] <- ifelse (init.surv[i, 4+j, ] == FALSE,
+                                       ifelse(init.surv[i, 2, ] == TRUE,
+                                              rbinom(length(suc), 1, suc),
+                                              rbinom(length(uns), 1, uns)),
+                                       1)                                       
+        # Ok, this whole loop says: 
+        # Starting from line with first ifelse(): If the most recent survey was not responded to by hunter i, (initial survey is column 4, and first loop j = 0)
+        # next line: and if hunter i harvested, 
+        # next line: re-evaluate if they answered this time around based off new scaled probabilities, 
+        # next line: If they didn't respond to any of the previous surveys, but didn't harvest, will they respond this time?
+        # next line: If they already responded to a previous survey don't survey them again and consider them responded. 
+        # then it loops back to the top for loop, rescaling response rates and doing that again and again until all FUS are done
+      }
+    }}
+  
+  # Creating Outputs:
+  df.list <- apply(init.surv, 3, as.data.frame)       # Make the survey matrices into a list of dataframes
+  TrueHvst <- sum (pop [ , 2])                        # True harvest of entire pop.
+  list <- list(n, TrueHvst, as.data.frame(pop))       # Combine pop. size, number of harvests, and entire population
+  names(list) <- c("n", "true harvest", "complete pop")
+  biglist <- c(list, df.list)                         # Combine the two lists
+  return(biglist)                                                                
 }
-########## SCENARIO 1: Simple Random Sample by phone w/ follow-up ###########################
 
-########## Example of functions: Large pop. w/ successful and unsuccessful hunters equally likely to report ##########
+####################### Examples of functions ##############################
+########## Large pop. w/ successful and unsuccessful hunters equally likely to respond, no follow up surveys ##########
 
-# Simulate a population of 10,000; split into groups of hunters with different rates of harvest (0.4 or 0.2) which are split evenly (for an average harvest rate of 30%) and all have a probability of being sampled = 0.5
-s1.Lpop <- popgen (n = 10000, pSuccess1 = 0.40, pSuccess2 = 0.20, pSample = 0.5)  
+# Simulate a population of 10,000; split into 2 groups of hunters with different rates of harvest (0.4 and 0.2 for an average harvest rate of 30%) and all have a probability of being sampled = 0.5.
+Lpop <- popgen (n = 10000, 
+                pSuccess1 = 0.40, 
+                pSuccess2 = 0.20, 
+                pSample = 0.5,
+                suc = seq (.2, 1, 0.1))  
 
-head (s1.Lpop[["total population"]])      # Reports Total population
-head (s1.Lpop[["sampled population"]])    # Reports Sampled population only
-s1.Lpop[["true harvest"]]                 # Reports total harvest
+Lpop[["n"]]                   # Reports Total population size
+Lpop[["true harvest"]]        # Reports total harvest of entire pop.
+head(Lpop[["complete pop"]])  # Reports dataframe of complete pop w/o survey columns filled in
+head(Lpop[["0.5 0.5"]], 12)   # Reports survey outcomes when both successful and unsuccessful hunters responded with prob. 0.5
+                              # Note, only sampled population contained in those dataframes
+apply(Lpop[["0.5 0.5"]], 2,  mean) # avg. of columns
 
+########## Medium pop. w/ successful and unsuccessful hunters not equally likely to respond, no follow up surveys ###########
 
+# Simulate a population of 1,000; split into groups of hunters with different rates of harvest (0.35 or 0.25 for an average harvest rate of 30%) and all have a probability of being sampled = 0.5, but successful hunters are 20% more likely to respond to surveys. 
 
-# Now input s1.Lpop to survgen() function to complete "response" column for varying levels of response
+Mpop.NRbias <- popgen (n = 1000, 
+                       pSuccess1 = 0.35, 
+                       pSuccess2 = 0.25, 
+                       pSample = 0.5,
+                       suc = seq (0.3, 0.9, 0.1), 
+                       uns = seq (0.1, 0.7, 0.1))  
 
-s1.Lpop.surv <- survgen (x = s1.Lpop, suc = seq (.2, 1, 0.1))   # Simulate response if population "s1.Lpop" responds to surveys with probabilities from 0.2 : 1 by increments of 0.1.
+Mpop.NRbias[["n"]]    
+Mpop.NRbias[["true harvest"]]       
+head(Mpop.NRbias[["0.4 0.2"]], 12)    # Reports simulation when successful hunters responded @ 40% and unsuccessful @ 20%
+apply(Mpop.NRbias[["0.6 0.4"]], 2, mean)
 
-# Since argument "uns" is left blank it defaults to the same as "suc", making no difference in response rates b/w successful and unsuccessful hunters.
+########## Medium pop. w/ unequal response rates and 3 follow up surveys ##########
 
-head (s1.Lpop.surv)                                   # Returns a different matrix for each level of response rate.
-head (s1.Lpop.surv[ , , "0.5"], 10)                   # Returns first 10 results for response rate of 0.5 only.
+# A population of 1,000 ........., with 3 follow up surveys conducted, each consecutive follow up survey being 50% less likely to hear a response from someone who didn't respond to any of the previous ones. 
 
+Lpop.NRbias.3FUS <- popgen (n = 1000,
+                            pSuccess1 = 0.4,
+                            pSuccess2 = 0.2,
+                            pSample = 0.5,
+                            suc = seq (0.3, 0.9, 0.1),
+                            uns = seq (0.1, 0.7, 0.1),
+                            FUS = 3,
+                            FUSscale = 0.5)
 
-for (i in 1:9){                                       # In this case, there are 9 levels of response.
-  print (mean(c(s1.Lpop.surv [ , 4, i])))             # Output of this loop should reflect those 9 levels (cont.)
-}                                                     # of the sequence put into survgen()
-
-
-########## Example: Medium pop. w/ successful and unsuccessful hunters not equally likely to report. ###########
-
-# First, simulate a population of 1,000; split into groups of hunters with different rates of harvest (0.35 or 0.25) which are split evenly (for an average harvest rate of 30%) and all have a probability of being sampled = 0.5
-
-s1.Mpop.NRbias <- popgen (n = 1000, pSuccess1 = 0.35, pSuccess2 = 0.20, pSample = 0.5)  
-
-head (s1.Mpop.NRbias[["total population"]])      # Reports Total population (dataframe)
-head (s1.Mpop.NRbias[["sampled population"]])    # Reports Sampled population only
-s1.Mpop.NRbias[["true harvest"]]                 # Reports total harvest
-
-
-
-# Now input s1.Mpop.NRbias to survgen() function to simulate response for varying levels of response
-
-s1.Mpop.NRbias.surv <- survgen (s1.Mpop.NRbias, suc = seq (0.3, 0.9, 0.1), uns = seq (0.1, 0.7, 0.1))   # Simulate response if population "s1.Mpop.NRbias" responds to surveys with different probabilities for successful and unsuccessful hunters
-
-head (s1.Mpop.NRbias.surv)                            # Returns a different matrix for each level of response rate.
-head (s1.Mpop.NRbias.surv[ , , "0.5"], 10)            # Returns first 10 results for response rate of 0.5 only. (relative to successful hunters)
-
-for (i in 1:7){                                       # In this case, there are 7 levels of response.
-  print (mean(c(s1.Mpop.NRbias.surv [ , 4, i])))      # Output of this loop should reflect somewhere in between given response rates, proportionate to harvest rates and nonresponse bias.
-}                                    
+Lpop.NRbias.3FUS[["n"]]
+head(Lpop.NRbias.3FUS[["0.3 0.1"]], 20)
+head(Lpop.NRbias.3FUS[["0.7 0.5"]], 12)
+head(Lpop.NRbias.3FUS[["complete pop"]])
+apply(Lpop.NRbias.3FUS[["0.6 0.4"]], 2, mean)
